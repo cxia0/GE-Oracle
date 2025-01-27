df385e185c4b4831279919592a39494dd1351783
Chunfeng Xia <chunfeng.xia21@gmail.com>
2025-01-27 13:40:20 +0000

    Create project

---
+++ GEOracle/GEOracleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GEOracleApp.swift
+//  GEOracle
+//
+//  Created by Chunfeng Xia on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct GEOracleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GEOracleTests/GEOracleTests.swift
@@ -0,0 +1,17 @@
+//
+//  GEOracleTests.swift
+//  GEOracleTests
+//
+//  Created by Chunfeng Xia on 27/01/2025.
+//
+
+import Testing
+@testable import GEOracle
+
+struct GEOracleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
